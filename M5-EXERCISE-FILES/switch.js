3a204440b193a2dc3eb40b6ae070ec20d1a6f415
Suman Dangi <sumankdangi26@gmail.com>
2026-01-14 14:23:40 +0545

    4th  Commit

--- M5-EXERCISE-FILES/switch.js
+++ M5-EXERCISE-FILES/switch.js
@@ -12,3 +12,19 @@ let employee = {
     salary: 60000
 };
 
+switch (employee.department) {
+    case "Engineering":
+        console.log('Meetings are on Monday at 10 AM.');
+        break;
+    case "Human Resources":
+        console.log('Meetings are on Tuesday at 11 AM.');
+        break;  
+    case "Marketing":
+        console.log('Meetings are on Wednesday at 2 PM.');
+        break;
+    case "Sales":
+        console.log('Meetings are on Thursday at 3 PM.');
+        break;
+    default:
+        console.log('Please check with your manager for meeting times.');
+}
